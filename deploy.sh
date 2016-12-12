724faa5b27a0218ff0d4287550d3b17bc403aea2
Soel <shachar.soel@sap.com>
2016-12-12 11:36:48 +0200

    @#$@#$@#$

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-#  git clone https://github.com/bd82/saptech15
-# cd saptech15
 mv .git /tmp/.
 git init
-git config --global user.email "shahar.soel@gmail.com"
-git config --global user.name "shahars"
+git config --global user.email "bamba.soel@gmail.com"
+git config --global user.name "bamba"
 git remote add origin https://P13887420:Abcd1234@git.hanatrial.ondemand.com/p13887420trial/saptech15
 git add .
 git add -u
-git commit -m “whatever”
+git commit -m “$TRAVIS_BUILD_NUMBER”
 git push origin master -f
