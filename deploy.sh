919d393d8ead22456df47614fef138880ebecab2
Soel <shachar.soel@sap.com>
2016-12-12 10:54:29 +0200

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 # git clone https://github.com/bd82/saptech15
 # cd saptech15
 rm –rf .git
 git remote add origin https://P13887420:Abcd1234@git.hana.ondemand.com/p13887420trial/saptech15
-it add .
+git add .
 git add -u
 git commit -m “whatever”
 git push origin master -f
