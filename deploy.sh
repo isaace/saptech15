9243130ff15f11c5a04572dc92e4e2a593f4cd91
Soel <shachar.soel@sap.com>
2016-12-12 11:18:02 +0200

    !!!!

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ mv .git /tmp/.
 git init
 git config --global user.email "shahar.soel@gmail.com"
 git config --global user.name "shahars"
-git remote add origin https://P13887420:Abcd1234@git.hana.ondemand.com/p13887420trial/saptech15
+git remote add origin https://P13887420:Abcd1234@git.hanatrial.ondemand.com/p13887420trial/saptech15
 git add .
 git add -u
 git commit -m “whatever”
