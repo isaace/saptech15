b76b4f61eda29290c3285bc09f5cfcaad178c1ba
isaace <isaac.eliassi@gmail.com>
2016-12-19 13:52:15 +0200

    use helium to activate the last deplyed version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#helium path
+HELIUM="/var/lib/helium"
+
+#push to the git repo
 mv .git /tmp/.
 git init
 git config --global user.email "bamba.soel@gmail.com"
@@ -9,3 +13,15 @@ git add .
 git add -u
 git commit -m “$TRAVIS_BUILD_NUMBER”
 git push origin master -f
+
+#Save the last commit id & timestamp
+GIT_COMMIT_ID=`git log --format="%H" -n 1`
+TIMESTAMP=`date +'%Y%m%d%H%M%S'`
+
+#deploying using the helium
+node ${HELIUM} addVersion saptech15 ${TIMESTAMP} ${GIT_COMMIT_ID} -a p13887420trial -u P13887420:Abcd1234 -s dispatcher.hanatrial.ondemand.com:443
+node ${HELIUM} activate saptech15 ${TIMESTAMP} -a p13887420trial -u P13887420:Abcd1234 -s dispatcher.hanatrial.ondemand.com:443
+
+#In case we need to start the app as well uncomment the line below
+#node ${HELIUM} start saptech15 -a p13887420trial -u P13887420:Abcd1234 -s dispatcher.hanatrial.ondemand.com:443
+
